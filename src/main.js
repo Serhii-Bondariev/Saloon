8dd105f11c6cae5b515e4cbfd782eb8a67ad96f5
Serhii-Bondariev <xx.code4u@gmail.com>
2023-09-28 00:13:59 +0300

    1

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './js/header/header';
+import './js/hero/hero';
+import './js/cocktailsMarkUp/cocktailsMarkUp';
+import './js/searchCocktails/searchCocktails';
+import './js/modalCocktails/modalCocktails';
+import './js/api/api';
